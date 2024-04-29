57c19e8f242728ca3e88b6373dc1e789a7765594
matouseek <matouszatloukal@seznam.cz>
2024-04-29 15:54:56 +0200

    Added movement

---
+++ Jeremy the Ghost/Assets/Scripts/JeremyController.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JeremyController : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D rb;
+    
+    [SerializeField] 
+    private float movementSpeed;
+
+    /// <summary>
+    /// These values cap the max velocity the player can move, it is to prevent him from stacking up force which
+    /// will make his velocity huge and boost him to infinity (Well not literally).
+    ///
+    /// Choosing 1 means the player can move in the direction again only if his velocity is less than what he gets
+    /// immediately after one move. (Basically allows him to move twice in one direction before having to wait).
+    ///
+    /// If you set new values, you have to restart the game as thr/thl/thu (which are thresholds in the velocity language)
+    /// are calculated at Start().
+    /// </summary>
+    [SerializeField] private float thresholdRightMove;
+    [SerializeField] private float thresholdLeftMove;
+    [SerializeField] private float thresholdUpMove;
+    private float thr;
+    private float thl;
+    private float thu;
+
+    private void Start()
+    { 
+        thr = movementSpeed * Time.fixedDeltaTime * thresholdRightMove;
+        thl = movementSpeed * Time.fixedDeltaTime * -thresholdLeftMove;
+        thu = movementSpeed * Time.fixedDeltaTime * thresholdUpMove;
+    }
+    
+    void Update()
+    {
+        Move();
+    }
+
+    Vector2 GetInputNormalized()
+    {
+        var input = Vector2.zero;
+        if (Input.GetKeyDown(KeyCode.D) && rb.velocity.x <= thr) { input += Vector2.right; }
+        if (Input.GetKeyDown(KeyCode.A) && rb.velocity.x >= thl) { input += Vector2.left; }
+        if (Input.GetKeyDown(KeyCode.W) && rb.velocity.y <= thu) { input += Vector2.up; }
+        return input;
+    }
+
+    void Move()
+    {
+        rb.AddForce(GetInputNormalized() * movementSpeed);
+    }
+}
