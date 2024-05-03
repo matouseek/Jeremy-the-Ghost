6dd74a1caf6b8c18c82bdbf5d55ff960be2142b7
matouseek <matouszatloukal@seznam.cz>
2024-05-03 09:53:49 +0200

    Add scare children camera

--- Jeremy the Ghost/Assets/Scripts/Children.cs
+++ Jeremy the Ghost/Assets/Scripts/Children.cs
@@ -7,15 +7,22 @@ public class Children : MonoBehaviour
 {
     [SerializeField] private GameObject scareChildrenText;
     [SerializeField] private JeremyController jeremy;
+
+    [SerializeField] private Cinemachine.CinemachineVirtualCamera previousCamera;
+    [SerializeField] private Cinemachine.CinemachineVirtualCamera jeremyAndChildrenCamera;
     
     private void OnTriggerEnter2D(Collider2D other)
     {
+        previousCamera.Priority = 10;
+        jeremyAndChildrenCamera.Priority = 100;
         scareChildrenText.SetActive(true);
         jeremy.CanScare = true;
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
+        previousCamera.Priority = 100;
+        jeremyAndChildrenCamera.Priority = 10;
         scareChildrenText.SetActive(false);
         jeremy.CanScare = false;
     }
